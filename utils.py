ffd9c30dc01e0acc3689e613e8035bc1d8e807e6
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-05-30 23:10:09 +0530

    🐛 fix block not containing contracts bug

--- utils.py
+++ utils.py
@@ -2,11 +2,14 @@ import json
 import requests
 import config
 
+
 def get_latest_block():
-  return requests.get(f"{config.NXS_BASE_URL}/ledger/list/blocks", params={"limit":1, "verbose":"detail"}).json()["result"][0]
+    return requests.get(f"{config.NXS_BASE_URL}/ledger/list/blocks", params={"limit": 1, "verbose": "detail"}).json()["result"][0]
+
 
 def get_block(height):
-  return requests.get(f"{config.NXS_BASE_URL}/ledger/get/block?height={height}").json()["result"]
+    return requests.get(f"{config.NXS_BASE_URL}/ledger/get/block?verbose=detail&height={height}").json()["result"]
+
 
 def pp(_json):
     json.dumps(_json, indent=1)
