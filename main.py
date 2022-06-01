6f4c28689c75f1c0c70f6725b544f97829fec648
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2022-06-01 22:41:47 +0530

    ♻️ ✨ refactor, block scan command

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import time
 import dotenv
 import strings
 import config
-import random
 import logging
 import threading
 from pyrogram import Client, filters
 
-from utils import get_block, get_latest_block
+from utils import get_block, get_latest_block, process_block
 dotenv.load_dotenv()
 
 
@@ -41,19 +40,17 @@ def handle_command(client, message):
     message.reply(main_context["last_block"])
 
 
-@bot.on_message(filters.command("debug"))
+@bot.on_message(filters.command("b"))
 def handle_command(client, message):
-    logging.info(message)
-    message.reply("debug")
-    amt = random.randint(0, 1000000)
-    msg = strings.whale_notification(main_context.get("last_block"), {
-                                     "amount": amt, "op": "CREDIT", "from": "sda", "proof": "das", "to": "sd9a", "token": "NXS"})
-    message.reply(msg)
+    blocknum: int = message.command[1]
+    print(f"/b {blocknum}")
+    block_json = get_block(blocknum)
+    responses = process_block(block_json)
+    for m in responses:
+        message.reply(m)
 
 
 def whale_notifier(main_context):
-    # global LAST_BLOCK
-
     def get_lost_block() -> int:
         "return None if no lost block or block number if block of the lost block"
         try:
@@ -67,46 +64,11 @@ def whale_notifier(main_context):
             print("get_lost_block ERROR: ", e)
         return None
 
-    def scan_and_send_alert(block):
-        def extract_info(contract):
-            try:
-                # ! only handles trituim txns, legacy amount is not included
-                amount = contract.get("amount")
-                token = contract.get("token")
-                tx_from = contract.get("from")
-                tx_to = contract.get("to")
-                proof = contract.get("proof")
-                op = contract.get("OP")
-                credit_for = contract.get("for")
-                if(op == "DEBIT" and config.HIDE_DEBIT_TXNS):
-                    return None
-
-                if str(token) in ["0", "0000000000000000000000000000000000000000000000000000000000000000"]:
-                    token = "NXS"
-
-                return {"amount": amount, "op": op, "for": credit_for, "from": tx_from, "proof": proof, "to": tx_to, "token": token,}
-            except Exception as e:
-                print(e)
-                logging.error("Extract info error: ", e)
-        try:
-            for tx in block.get("tx"):
-                contracts = tx.get("contracts")
-                if contracts is not None:
-                    for contract in contracts:
-                        contract_info = extract_info(contract)
-                        amount = contract_info.get("amount")
-                        if amount is not None:
-                            if amount > config.ALERT_AMOUNT:
-                                block_height = block.get("height")
-                                message = strings.whale_notification(
-                                    block_height, contract_info)
-                                bot.send_message(
-                                    config.DEVELOPER_CHAT_ID if config.DEBUG_MODE else config.ALERT_CHANNEL_ID, message)
-                        else:
-                            logging.error(
-                                f"contract parsing failed for a contact: {contract} in block: {block.get('height')}")
-        except:
-            logging.error(f"block parsing failed for {block.get('height')}")
+    def scan_and_send_alert(block: json):
+        responses = process_block(block)
+        for msg in responses:
+            bot.send_message(
+                config.DEVELOPER_CHAT_ID if config.DEBUG_MODE else config.ALERT_CHANNEL_ID, msg)
 
     logging.info("Starting whale notifier thread.")
     while True:
--- utils.py
+++ utils.py
@@ -1,15 +1,62 @@
 import json
-import requests
 import config
-
+import strings
+import logging
+import requests
 
 def get_latest_block():
     return requests.get(f"{config.NXS_BASE_URL}/ledger/list/blocks", params={"limit": 1, "verbose": "detail"}).json()["result"][0]
 
-
 def get_block(height):
     return requests.get(f"{config.NXS_BASE_URL}/ledger/get/block?verbose=detail&height={height}").json()["result"]
 
-
 def pp(_json):
     json.dumps(_json, indent=1)
+
+def extract_contract_info(contract) -> dict:
+    try:
+        # ! only handles trituim txns, legacy amount is not included
+        amount = contract.get("amount")
+        token = contract.get("token")
+        tx_from = contract.get("from")
+        tx_to = contract.get("to")
+        proof = contract.get("proof")
+        op = contract.get("OP")
+        credit_for = contract.get("for")
+        if(op == "DEBIT" and config.HIDE_DEBIT_TXNS):
+            return None
+
+        if str(token) in ["0", "0000000000000000000000000000000000000000000000000000000000000000"]:
+            token = "NXS"
+
+        return {"amount": amount, "op": op, "for": credit_for, "from": tx_from, "proof": proof, "to": tx_to, "token": token, }
+    except Exception as e:
+        print(e)
+        logging.error("Extract contract info error: ", e)
+
+def process_block(block: json) -> list:
+    messages = []
+    errors = []
+    try:
+        for tx in block.get("tx"):
+            contracts = tx.get("contracts")
+            if contracts is not None:
+                for contract in contracts:
+                    contract_info = extract_contract_info(contract)
+                    amount = contract_info.get("amount")
+                    if amount is not None:
+                        block_height = block.get("height")
+                        message = strings.whale_notification(
+                            block_height, contract_info)
+                        messages.append(message)
+                    else:
+                        errors.append(
+                            f"contract parsing failed for a contact: {contract} in block: {block.get('height')}")
+    except:
+        errors.append(f"block parsing failed for {block.get('height')}")
+    
+    # * log errors
+    for error in errors:
+        logging.error(error)
+    
+    return messages
